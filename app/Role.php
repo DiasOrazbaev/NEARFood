1362761b00649bc95ede0cfb6cc2ad3a339a03d1
Dias <orazbaevdev@gmail.com>
2022-05-14 09:10:22 +0600

    First Commit

---
+++ app/Role.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Role extends Model
+{
+    use SoftDeletes;
+
+    public $table = 'roles';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'title',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+}
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<div class="fullwidth-sidebar-container">
+    <div class="sidebar bottom-sidebar"></div>
+</div>
+<footer id="footer" class="site-footer" role="contentinfo">
+    <div class="copyright">
+        <div class="container">
+            <p class="copyright-text"> 2022 Dias Orazbaev </p>
+        </div>
+    </div>
+</footer>
